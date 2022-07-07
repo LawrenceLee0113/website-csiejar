7f7d9053d1dea71d07ab7708d1ca8c2447ea60e1
LawrenceLee0113 <87930236+LawrenceLee0113@users.noreply.github.com>
2022-07-07 14:09:21 +0800

    07071409

--- static/script/error_modal.js
+++ static/script/error_modal.js
@@ -1,3 +1,40 @@
+function return_img_uploader(finish_func){
+
+  //上傳 img 檔案至 image kit
+  let new_img_url = ""
+  imagekit_uploader({
+      file_id:"return_img_uploader",
+      user_id:user.user_id,
+      user_token:user.user_token,
+      file_name: "error_"+user.user_id,
+      folder_name:"/error",
+      file_type:"png",
+      status_func:{
+          status0:()=>{//req send(self server)
+              console.log("status0")
+              progress_controller("return_img_progress",25)
+          },
+          status1:(body)=>{//res catch(self server)
+              console.log("status1")
+              change_user_token(body.user.user_token)
+              progress_controller("return_img_progress",50)
+              
+          },
+          status2:()=>{//req send(imgkit server)
+              console.log("status2")
+          },
+          status3:(body)=>{//res catch(imgkit server)
+              progress_controller("return_img_progress",75)
+              console.log("status3")
+              console.log(body)
+          },
+          status4:()=>{//finish
+              finish_func()
+              console.log("status4")
+          }
+      }
+  })
+}
 $(document).ready(function () {
   let option_template = $("#btnGroupDrop1_li a");
   let output_html = ""
@@ -16,27 +53,40 @@ $(document).ready(function () {
   });
   $("#return_submit").click(function (e) {
     // 送出回報錯誤表單
-    e.preventDefault();
-    let return_obj = {
-      "return_page_input": $("#return_page_input").val(),
-      "return_title_input": $("#return_title_input").val(),
-      "return_content_input": $("#return_content_input").val(),
-      "return_img_url_input": $("#return_img_url_input").val(),
-      "return_mail_input": $("#return_mail_input").val()
-    }
-    console.log(
-      $("#return_page_input").val(),
-      $("#return_title_input").val(),
-      $("#return_content_input").val(),
-      $("#return_img_url_input").val(),
-      $("#return_mail_input").val()
-    )
-    $.post("/return", { return_obj: return_obj },
-      function (data, textStatus, jqXHR) {
-        console.log(data)
-      },
-      "json"
-    );
+    imagekit_uploader(()=>{
+      let return_obj = {
+        "return_page_input": $("#return_page_input").val(),
+        "return_title_input": $("#return_title_input").val(),
+        "return_content_input": $("#return_content_input").val(),
+        "return_img_url_input": $("#return_img_url_input").val(),
+        "return_mail_input": $("#return_mail_input").val()
+      }
+      console.log(
+        $("#return_page_input").val(),
+        $("#return_title_input").val(),
+        $("#return_content_input").val(),
+        $("#return_img_url_input").val(),
+        $("#return_mail_input").val()
+      )
+      $.post("/return", { return_obj: return_obj },
+        function (data, textStatus, jqXHR) {
+          progress_controller("return_img_progress",100)
+
+          console.log(data)
+        },
+        "json"
+      );
+
+    })
+    
   });
+  $("#return_img_uploader_btn").click(function (e) {
+    //報錯 img 檔案的按鈕
+    $('#return_img_uploader').trigger('click');
+  });
+
+
+  view_img("return_img_uploader","return_img_uploader_container_b")
+
 });
-progress_controller("return_img_progress", 87)//測試 上傳img
+// progress_controller("return_img_progress", 87)//測試 上傳img
